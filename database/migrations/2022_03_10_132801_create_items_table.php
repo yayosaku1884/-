f20d43ed945ffb5ab89ac17443f8caca10268f97
yayosaku1884 <ibayashi0925@gmail.com>
2022-11-05 15:19:43 +0900

    charset 追加

--- database/migrations/2022_03_10_132801_create_items_table.php
+++ database/migrations/2022_03_10_132801_create_items_table.php
@@ -19,7 +19,7 @@ class CreateItemsTable extends Migration
             $table->string('name', 100)->index();
             $table->string('status', 100)->default('active');
             $table->smallInteger('type')->nullable();
-            $table->string('detail', 500)->nullable();
+            $table->string('detail', 200)->nullable();
             $table->timestamps();
         });
     }
